4dbcd8d6f6850bdecaeca3cae55efacc7e32bff1
Daniel Labarca <daniel.labarca@outlook.com>
2024-01-14 13:43:11 -0300

    commit inicial

--- app/src/main/java/com/example/evaluacion3/MainActivity.kt
+++ app/src/main/java/com/example/evaluacion3/MainActivity.kt
@@ -3,6 +3,20 @@ package com.example.evaluacion3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+enum class Pantalla {           // MENU pantallas
+    FORM,
+    CAMARA
+}
+
+class AppVM : ViewModel() {
+    val pantallaActual = mutableStateOf(Pantalla.FORM)
+
+
+    var onPermisoCamaraOk:() -> Unit = {}
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
