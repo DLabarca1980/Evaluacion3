f3d4a94819cef0499c5d78a8babb500fcdec1bf7
Daniel Labarca <daniel.labarca@outlook.com>
2024-01-14 18:26:09 -0300

    commit inicial

--- app/src/main/java/com/example/evaluacion3/MainActivity.kt
+++ app/src/main/java/com/example/evaluacion3/MainActivity.kt
@@ -20,9 +20,14 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -31,6 +36,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -196,7 +202,7 @@ fun AppUI(cameraController: CameraController){
     Pantalla.FOTO -> {
         PantallaFotoUI(formRecepcionVM, cameraController, cameraController)
     }
-    else -> {
+    else -> run {
         Log.v("AppUI()", "when else, no deberia entrar aqui")
     }
   }
@@ -228,10 +234,24 @@ fun PatallaFormUI(
             Text("Tomar Fotografia")
         }
         formRecepcionVM.fotoUbicacion.value?.also {
+            Box( Modifier.size(200.dp, 100.dp) ) {
+                Image(painter = BitmapPainter(uri2imageBitmap(it, contexto)),
+                    contentDescription = "Imagen "
 
+                )
+            }
         }
+        Text("La ubicacion es : lat: ${formRecepcionVM.latitud.value} y"
+                          " long: ${formRecepcionVM.longitud.value}  ")
+        Button(onClick = {
+            actualizarUbicacionOnclick()
+
+        }) {
+            Text("Actualizar Ubicacion")
+        }
+        Spacer(Modifier.height(100.dp))
     }
-}
+} // SEGUIR EN LA LINEA 240 ejercicioSemana6
 
 
 
