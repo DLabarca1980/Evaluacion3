9f0dd8aaa152fa67576829edd6a1d763472f01b8
Daniel Labarca <daniel.labarca@outlook.com>
2024-01-15 15:04:48 -0300

    commit inicial

--- app/src/main/java/com/example/evaluacion3/MainActivity.kt
+++ app/src/main/java/com/example/evaluacion3/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.evaluacion3
 
+import android.Manifest 
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.health.connect.datatypes.ExerciseRoute
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
@@ -25,6 +25,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -45,6 +46,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 import java.io.File
 import java.time.LocalDateTime
 
@@ -54,6 +60,9 @@ enum class Pantalla {           // MENU pantallas
 }
 
 class CameraAppViewModel : ViewModel() {
+    val latitud   = mutableStateOf(0.0)
+    val longitud  = mutableStateOf(0.0)
+
     val pantalla = mutableStateOf(Pantalla.FORM)
 
     var onPermisoCamaraOK    : () -> Unit = {}
@@ -161,7 +170,7 @@ fun capturarFoto(
 
 class SinPermisoException(mensaje:String): Exception(mensaje)
 
-fun getUbicacion(contexto: Context, onUbicacionOK:(location: ExerciseRoute.Location) ->Unit):Unit {
+fun getUbicacion(contexto: Context, onUbicacionOK:(location: android.location.Location) ->Unit):Unit {
     try {
         val servicio = LocationServices.getFusedLocationProviderClient(contexto)
         val tarea = servicio.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
@@ -177,40 +186,39 @@ fun getUbicacion(contexto: Context, onUbicacionOK:(location: ExerciseRoute.Locat
 fun AppUI(cameraController: CameraController){
     val contexto = LocalContext.current
 
-    val formRecepcionVM : FormRecepcionViewModel = viewModel()
+    val formRecepcionVM : CameraAppViewModel = viewModel()
     val cameraAppViewModel : CameraAppViewModel  = viewModel()
 
     when (cameraAppViewModel.pantalla.value) {
         Pantalla.FORM -> {
             PantallaFormUI(
                 formRecepcionVM,
-                tomarFotoClick = {
+                tomarFotoOnClick = {
                     cameraAppViewModel.cambiarPantallaFoto()
-                    cameraAppViewModel.lanzadorPermisos?.launch(arrayOf(Manifest.permission.CAMERA))   // TOMA ERROR REVISARR!!!!
+                    cameraAppViewModel.lanzadorPermisos?.launch(arrayOf(Manifest.permission.CAMERA))
                 },
-                actualizarUbicacionOnClick = {
+                actualizarUbicacionOnclick = {
                    getUbicacion(contexto) {
                        formRecepcionVM.latitud.value = it.latitude
                        formRecepcionVM.longitud.value = it.longitude
                    }
-                }
-                cameraAppViewModel.lanzadorPermisos?.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                    cameraAppViewModel.lanzadorPermisos?.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION))
-        }
+                }
     )
 }
     Pantalla.FOTO -> {
-        PantallaFotoUI(formRecepcionVM, cameraController, cameraController)
-    }
-    else -> run {
+        PantallaFotoUI(formRecepcionVM, cameraAppViewModel, cameraController)
+    } else -> run {
         Log.v("AppUI()", "when else, no deberia entrar aqui")
+        }
     }
-  }
 }
 
 @Composable
-fun PatallaFormUI(
-    formRecepcionVM : FormRecepcionViewModel, tomarFotoOnClick:() -> Unit = {},
+fun PantallaFormUI(
+    formRecepcionVM : CameraAppViewModel,
+    tomarFotoOnClick:() -> Unit = {},
     actualizarUbicacionOnclick:() -> Unit = {}
 ) {
     val contexto = LocalContext.current
@@ -219,30 +227,36 @@ fun PatallaFormUI(
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         TextField(
-            label = { Text("Ubicación")},   // DECIA RECEPTOR
-            value = formRecepcionVM.ubicacion.value ,
-            onValueChange = { formRecepcionVM.ubicacion.value = it },
+            label = { Text("latitud")},                // DECIA RECEPTOR
+            value = formRecepcionVM.latitud.value.toString(),
+            onValueChange = { newValue -> formRecepcionVM.latitud.value = newValue.toDoubleOrNull() ?: formRecepcionVM.latitud.value},
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 10.dp)
         )
-        Text("Fotografia de la ubicación")    // decia esto Text("Fotografia de la recepcion de la encomienda: ")
-        Button(onClick = {
-            tomarFotoOnClick()
-        }
-        ) {
-            Text("Tomar Fotografia")
+        TextField(  label = { Text("Longitud")},
+                    value = formRecepcionVM.longitud.value.toString(),
+                    onValueChange = { newValue -> formRecepcionVM.longitud.value = newValue.toDoubleOrNull() ?: formRecepcionVM.longitud.value },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp)
+//        Text("Fotografia de la ubicación")    // decia esto Text("Fotografia de la recepcion de la encomienda: ")
+//        Button(onClick = {
+//            tomarFotoOnClick()
+        )
+        Button(onClick = tomarFotoOnClick) {
+            Text(" Tomar Fotografía")
         }
         formRecepcionVM.fotoUbicacion.value?.also {
             Box( Modifier.size(200.dp, 100.dp) ) {
-                Image(painter = BitmapPainter(uri2imageBitmap(it, contexto)),
+                Image(
+                    painter = BitmapPainter(uri2imageBitmap(it, contexto)),
                     contentDescription = "Imagen "
-
                 )
             }
         }
-        Text("La ubicacion es : lat: ${formRecepcionVM.latitud.value} y"
-                          " long: ${formRecepcionVM.longitud.value}  ")
+        Text("La ubicacion es : lat: ${formRecepcionVM.latitud.value} y",
+            " long: ${formRecepcionVM.longitud.value}  ")
         Button(onClick = {
             actualizarUbicacionOnclick()
 
@@ -251,48 +265,102 @@ fun PatallaFormUI(
         }
         Spacer(Modifier.height(100.dp))
     }
-} // SEGUIR EN LA LINEA 240 ejercicioSemana6
 
-
-
-
-
-
+}
 
 @Composable
-fun PantallaCamaraUI(
-    lanzadorPermisos: ActivityResultLauncher<Array<String>>,
-    cameraController: LifecycleCameraController
-) {
+fun PantallaFotoUI(formRecepcionVM  : CameraAppViewModel,
+                   appViewModel     : CameraAppViewModel,
+                   cameraController : CameraController) {
     val contexto = LocalContext.current
-    val formRegistroVM:FormRecepcionVM = viewModel()
-    val appVM:AppWM                    = viewModel()
-
-    lanzadorPermisos.launch(arrayOf(android.Manifest.permission.CAMERA) )
 
     AndroidView(
-        modifier = Modifier.fillMaxSize(),
         factory = {
-            PreviewView(it).apply {
-                controller = cameraController
+            PreviewView(it).apply { controller = cameraController
             }
-        }
-    )
+        },
+        modifier = Modifier.fillMaxSize()
+        )
     Button(onClick = {
-        capturarFoto(
-            cameraController,
-            crearArchivoImagenPrivada(contexto),
-            contexto
-        ) {
-          formRegistroVM.fotoUbicacion.value = it
-            appVM.pantallaActual.value = Pantalla.FORM
-        }
+            capturarFoto(
+                capturarFoto, crearArchivoImagenPrivada(contexto),
+                contexto
+            ) {
+                formRecepcionVM.fotoUbicacion.value = it
+            appViewModel.cambiarPantallaFoto()
+            }
 
     }) {
-        Text("Capturar Foto")
-
+        Text("Tomar Foto")
     }
 }
+
+@Composable
+fun MapaOsmUI(latitud:Double, longitud:Double) {
+    val contexto = LocalContext.current
+
+    AndroidView(
+        factory = {
+            MapView(it).also {
+                it.setTileSource(TileSourceFactory.MAPNIK)
+                Configuration.getInstance().userAgentValue = contexto.packageName
+            }
+        }, update = {
+            it.overlays.removeIf { true }
+            it.invalidate()
+
+            it.controller.setZoom(18.0)
+            val geoPoint = GeoPoint(latitud, longitud)
+            it.controller.animateTo(geoPoint)
+
+            val marcador = Marker(it)
+            marcador.position = geoPoint
+            marcador.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+            it.overlays.add(marcador)
+        }
+    )
+}
+
+
+
+
+
+
+
+//@Composable
+//fun PantallaCamaraUI(
+//    lanzadorPermisos: ActivityResultLauncher<Array<String>>,
+//    cameraController: LifecycleCameraController
+//) {
+//    val contexto = LocalContext.current
+//    val formRegistroVM:FormRecepcionVM = viewModel()
+//    val appVM:AppWM                    = viewModel()
+//
+//    lanzadorPermisos.launch(arrayOf(android.Manifest.permission.CAMERA) )
+//
+//    AndroidView(
+//        modifier = Modifier.fillMaxSize(),
+//        factory = {
+//            PreviewView(it).apply {
+//                controller = cameraController
+//            }
+//        }
+//    )
+//    Button(onClick = {
+//        capturarFoto(
+//            cameraController,
+//            crearArchivoImagenPrivada(contexto),
+//            contexto
+//        ) {
+//          formRegistroVM.fotoUbicacion.value = it
+//            appVM.pantallaActual.value = Pantalla.FORM
+//        }
+//
+//    }) {
+//        Text("Capturar Foto")
+//
+//    }
+//}
 
 
 
